732d9c8d4d30b87568580df0bcc9e0899f0dd11f
Ashish Singh <ashishcodes4@gmail.com>
2018-06-23 08:17:39 +0530

    adding database, server responding

--- server.js
+++ server.js
@@ -5,14 +5,27 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "apple@123",
-    insecureAuth : true
-  });
+    password: "password",
+    port: 3306,
+    debug: true,
+    database: 'demo'
+});
+
+// mysql.createPool({
+//     connectionLimit : 500, //important
+//     host     : 'localhost',
+//     port     : 8889,
+//     user     : 'root',
+//     password : 'root',
+//     database : 'testing_db',
+//     debug    :  false,
+//     charset  : "utf8mb4_general_ci",
+//     supportBigNumbers: true,
+//     bigNumberStrings : true,
+// });
   
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
   });
-  app.listen('3000', function() {
-    console.log('connected');
-  });
+
